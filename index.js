799ab0ba02c831b70e149665b5d9b70ed06f4f40
Ruslan <gnatisin.ruslan1988@gmail.com>
2024-09-20 11:10:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true, 
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  mousewheel: {
+    invert: false,
+  },
+})
